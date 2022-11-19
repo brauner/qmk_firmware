715825747940cbad1cc771214e04a02502fcd372
Christian Brauner <brauner@kernel.org>
2022-11-19 15:34:42 +0100

    keyboards/preonic/keymaps/braunerized: update
    Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>

--- keyboards/preonic/keymaps/braunerized/keymap.c
+++ keyboards/preonic/keymaps/braunerized/keymap.c
@@ -38,24 +38,24 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |Layer1|AltGr |Raise |Lower | Alt  |Space |Lower |Raise |AltGr |Layer1| Ctrl |
- * `-----------------------------------------------------------------------------------'
+ * ,---------------------------------------------------------------------------------------------------------.
+ * |   `       |   1  |   2  |   3       |   4        |   5  |   6  |   7        |   8  |   9  |   0  | Bksp |
+ * |-----------+------+------+-----------+------------+------+------+------------+------+------+------+------|
+ * | Tab/Ctrl  |   Q  |   W  |   E       |   R        |   T  |   Y  |   U        |   I  |   O  |   P  | Ctrl |
+ * |-----------+------+------+-----------+------------+-------------+------------+------+------+------+------|
+ * | Esc       |   A  |   S  |   D       |   F        |   G  |   H  |   J        |   K  |   L  |   ;  |Enter |
+ * |-----------+------+------+-----------+------------+------|------+------------+------+------+------+------|
+ * |Shift      |   Z  |   X  |   C       |   V        |   B  |   N  |   M        |   ,  |   .  |   /  |Shift |
+ * |-----------+------+------+------+----+------------+------+------+------------+------+------+------|------|
+ * |           |Layer1|AltGr |Raise/Bksp |Lower/Enter | Alt  |  Esc |Lower/Space |Raise |AltGr |Layer1|      |
+ * `---------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,   KC_2,    KC_3,  KC_4,      KC_5,    KC_6,    KC_7,   KC_8,      KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,   KC_W,    KC_E,  KC_R,      KC_T,    KC_Y,    KC_U,   KC_I,      KC_O,    KC_P,    _______,
-  _______, KC_A,   KC_S,    KC_D,  KC_F,      KC_G,    KC_H,    KC_J,   KC_K,      KC_L,    KC_SCLN, _______,
-  KC_LSFT, KC_Z,   KC_X,    KC_C,  KC_V,      KC_B,    KC_N,    KC_M,   KC_COMM,   KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, LAYER1, KC_ALGR, RAISE, LT(_LOWER, KC_ESC), KC_LALT, KC_SPC, LT(_LOWER, KC_ENT), RAISE,   KC_ALGR, LAYER1, KC_RCTL
+  KC_GRV,          KC_1,   KC_2,    KC_3,                KC_4,               KC_5,    KC_6,   KC_7,               KC_8,    KC_9,    KC_0,    _______,
+  LCTL_T(KC_TAB),  KC_Q,   KC_W,    KC_E,                KC_R,               KC_T,    KC_Y,   KC_U,               KC_I,    KC_O,    KC_P,    KC_RCTL
+  _______,         KC_A,   KC_S,    KC_D,                KC_F,               KC_G,    KC_H,   KC_J,               KC_K,    KC_L,    KC_SCLN, _______,
+  KC_LSFT,         KC_Z,   KC_X,    KC_C,                KC_V,               KC_B,    KC_N,   KC_M,               KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  _______,         LAYER1, KC_ALGR, LT(_RAISE, KC_BSPC), LT(_LOWER, KC_ENT), KC_LALT, KC_ESC, LT(_LOWER, KC_SPC), RAISE,   KC_ALGR, LAYER1,  _______
 ),
 
 /* Layer1
