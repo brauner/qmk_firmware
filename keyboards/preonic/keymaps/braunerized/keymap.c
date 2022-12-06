f6b0905cecce0a567abbaf7b64c8dc9c03682a08
Christian Brauner <brauner@kernel.org>
2022-12-06 09:57:18 +0100

    keyboards/preonic/keymaps/braunerized: simplify usage of dedicated management layer
    Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>

--- keyboards/preonic/keymaps/braunerized/keymap.c
+++ keyboards/preonic/keymaps/braunerized/keymap.c
@@ -24,7 +24,7 @@ enum preonic_layers {
   _LOWER,
   _RAISE,
   _MOUSE1,
-  _ADJUST
+  _MANAGE
 };
 
 enum preonic_keycodes {
@@ -34,6 +34,7 @@ enum preonic_keycodes {
   LOWER,
   RAISE,
   MOUSE1,
+  MANAGE,
   BACKLIT
 };
 
@@ -61,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL,          KC_Q,    KC_W,    KC_E,  KC_R,              KC_T,    KC_Y,        KC_U,              KC_I,    KC_O,   KC_P,    KC_RCTL,
   MOD_TAP_LSFT_ESC, KC_A,    KC_S,    KC_D,  KC_F,              KC_G,    KC_H,        KC_J,              KC_K,    KC_L,   KC_QUOT, MOD_TAP_LSFT_ENT,
   KC_LEAD,          KC_Z,    KC_X,    KC_C,  KC_V,              KC_B,    KC_N,        KC_M,              KC_COMM, KC_DOT, KC_SLSH, KC_LEAD,
-  _______,          _______, LAYER2,  RAISE, MOD_TAP_LOWER_SPC, KC_LALT, TG(_MOUSE1), MOD_TAP_LOWER_SPC, RAISE,   LAYER2, _______, _______
+  _______,          _______, LAYER2,  RAISE, MOD_TAP_LOWER_SPC, KC_LALT, TG(_MOUSE1), MOD_TAP_LOWER_SPC, RAISE,   LAYER2, _______, MANAGE
 ),
 
 /* Layer1
@@ -168,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX, XXXXXXX,       XXXXXXX,       XXXXXXX,     XXXXXXX,        XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, KC_MS_ACCEL2, KC_MS_ACCEL1, KC_MS_ACCEL0, XXXXXXX, XXXXXXX,       XXXXXXX,       XXXXXXX,     XXXXXXX,        XXXXXXX, XXXXXXX),
 
-/* Adjust (Lower + Raise)
+/* Manage
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -178,15 +179,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |Reset |      |
+ * |Reset |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_preonic_grid(
+[_MANAGE] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, MU_MOD,  AU_ON,   AU_OFF,  XXXXXXX, XXXXXXX, QWERTY,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX
+  QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 )
 };
 
@@ -217,20 +218,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case LOWER:
           if (record->event.pressed) {
             layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
           } else {
             layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
           }
           return false;
           break;
         case RAISE:
           if (record->event.pressed) {
             layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
           } else {
             layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return false;
+          break;
+        case MANAGE:
+          if (record->event.pressed) {
+            layer_on(_MANAGE);
+          } else {
+            layer_off(_MANAGE);
           }
           return false;
           break;
@@ -295,9 +300,9 @@ bool dip_switch_update_user(uint8_t index, bool active) {
     switch (index) {
         case 0:
             if (active) {
-                layer_on(_ADJUST);
+                layer_on(_MANAGE);
             } else {
-                layer_off(_ADJUST);
+                layer_off(_MANAGE);
             }
             break;
         case 1:
