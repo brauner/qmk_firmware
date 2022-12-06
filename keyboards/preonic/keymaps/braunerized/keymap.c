e4fca322cb19becd9bbcda0d42b42e3ce0c5d5af
Christian Brauner <brauner@kernel.org>
2022-12-06 11:31:14 +0100

    keyboards/preonic/keymaps/braunerized: remove layer tap
    The layer tap combination for the left hand side doesn't work nicely.
    I'm constantly running into the issue where I'm typing a space instead
    of switching layers.
    
    Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>

--- keyboards/preonic/keymaps/braunerized/keymap.c
+++ keyboards/preonic/keymaps/braunerized/keymap.c
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_CAPS,          KC_1,    KC_2,    KC_3,  KC_4,              KC_5,    KC_6,        KC_7,              KC_8,    KC_9,   KC_0,    _______,
-  KC_LCTL,          KC_Q,    KC_W,    KC_E,  KC_R,              KC_T,    KC_Y,        KC_U,              KC_I,    KC_O,   KC_P,    KC_RCTL,
-  MOD_TAP_LSFT_ESC, KC_A,    KC_S,    KC_D,  KC_F,              KC_G,    KC_H,        KC_J,              KC_K,    KC_L,   KC_QUOT, MOD_TAP_LSFT_ENT,
-  KC_LEAD,          KC_Z,    KC_X,    KC_C,  KC_V,              KC_B,    KC_N,        KC_M,              KC_COMM, KC_DOT, KC_SLSH, KC_LEAD,
-  _______,          _______, LAYER2,  RAISE, MOD_TAP_LOWER_SPC, KC_LALT, TG(_MOUSE1), MOD_TAP_LOWER_SPC, RAISE,   LAYER2, _______, MANAGE
+  KC_CAPS,          KC_1,    KC_2,    KC_3,  KC_4,  KC_5,    KC_6,        KC_7,              KC_8,    KC_9,   KC_0,    _______,
+  KC_LCTL,          KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,    KC_Y,        KC_U,              KC_I,    KC_O,   KC_P,    KC_RCTL,
+  MOD_TAP_LSFT_ESC, KC_A,    KC_S,    KC_D,  KC_F,  KC_G,    KC_H,        KC_J,              KC_K,    KC_L,   KC_QUOT, MOD_TAP_LSFT_ENT,
+  KC_LEAD,          KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,    KC_N,        KC_M,              KC_COMM, KC_DOT, KC_SLSH, KC_LEAD,
+  _______,          _______, LAYER2,  RAISE, LOWER, KC_LALT, TG(_MOUSE1), MOD_TAP_LOWER_SPC, RAISE,   LAYER2, _______, MANAGE
 ),
 
 /* Umlaut
